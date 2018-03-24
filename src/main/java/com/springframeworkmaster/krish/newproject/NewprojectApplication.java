8561193c0a751b8b37549e37de87abe343a05c96
sk <sskrishnas@gmail.com>
2018-03-23 21:03:36 -0500

    First Commit

---
+++ src/main/java/com/springframeworkmaster/krish/newproject/NewprojectApplication.java
@@ -0,0 +1,12 @@
+package com.springframeworkmaster.krish.newproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojectApplication.class, args);
+	}
+}
